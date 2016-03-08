285847eebbd03f6731ffb1c01021a0d5e9233f75
Jordi Prats <jprats@systemadmin.es>
2016-03-08 11:03:24 +0100

    llicencia

--- lib/puppet/parser/functions/bool2yesno.rb
+++ lib/puppet/parser/functions/bool2yesno.rb
@@ -1,3 +1,14 @@
+#shamesly stolen from: https://github.com/puppetlabs/puppetlabs-apache/blob/master/lib/puppet/parser/functions/bool2httpd.rb
+# with minor changes
+#
+#
+#Copyright (C) 2012 Puppet Labs Inc
+#
+#Puppet Labs can be contacted at: info@puppetlabs.com
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#
+#
 Puppet::Parser::Functions::newfunction(:bool2yesno, :type => :rvalue, :doc => <<-EOS
 Transform a supposed boolean to yes or no. Pass all other values through.
 Given a nil value (undef), bool2yesno will return 'no'
