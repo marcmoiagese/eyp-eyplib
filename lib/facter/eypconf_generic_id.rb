961973baa27c2c220b36aa7cc8a5279507887162
Jordi Prats <jordi.prats@gmail.com>
2020-03-30 16:21:59 +0200

    cometes

--- lib/facter/eypconf_generic_id.rb
+++ lib/facter/eypconf_generic_id.rb
@@ -1,27 +1,27 @@
 Dir.entries("/etc/eypconf/id").select {|f| !File.directory? f}.each do |i|
 
-  genetic_id = Facter::Util::Resolution.exec('bash -c \'cat #{i}\'').to_s
+  genetic_id = Facter::Util::Resolution.exec("bash -c 'cat #{i}'").to_s
 
   unless genetic_id.nil? or genetic_id.empty?
-    Facter.add('eypconf_#{i}') do
+    Facter.add("eypconf_#{i}") do
         setcode do
           genetic_id
         end
     end
 
-    Facter.add('eypconf_#{i}|_uppercase') do
+    Facter.add("eypconf_#{i}|_uppercase") do
         setcode do
           genetic_id.upcase
         end
     end
 
-    Facter.add('eypconf_#{i}_lowercase') do
+    Facter.add("eypconf_#{i}_lowercase") do
         setcode do
           genetic_id.downcase
         end
     end
 
-    Facter.add('eypconf_#{i}_source') do
+    Facter.add("eypconf_#{i}_source") do
         setcode do
           i
         end
