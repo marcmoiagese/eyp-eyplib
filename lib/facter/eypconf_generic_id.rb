2f1ff647b7472771c830d24f57a78117c1df3ebc
Jordi Prats <jordi.prats@gmail.com>
2020-03-30 15:48:21 +0200

    prova id

---
+++ lib/facter/eypconf_generic_id.rb
@@ -0,0 +1,25 @@
+Dir.entries("/etc/eypconf/id").select {|f| !File.directory? f}.each do |i|
+
+  genetic_id = Facter::Util::Resolution.exec('bash |i|').to_s
+
+  unless genetic_id.nil? or genetic_id.empty?
+    Facter.add('eypconf_|i|') do
+        setcode do
+          genetic_id
+        end
+    end
+
+    Facter.add('eypconf_|i|_uppercase') do
+        setcode do
+          genetic_id.upcase
+        end
+    end
+
+    Facter.add('eypconf_|i|_lowercase') do
+        setcode do
+          genetic_id.downcase
+        end
+    end
+  end
+
+end
